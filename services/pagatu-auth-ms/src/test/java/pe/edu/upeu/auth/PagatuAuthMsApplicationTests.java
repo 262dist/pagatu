db5785d08802e4669bc546eae6a7ebc8f33a1f28
sullon <sullondev@gmail.com>
2026-09-20 20:50:29 -0500

    S07

---
+++ services/pagatu-auth-ms/src/test/java/pe/edu/upeu/auth/PagatuAuthMsApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagatuAuthMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
